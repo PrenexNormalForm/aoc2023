dfbf44c67a89e36e20ccce2a21bf9dd2ecf29b13
PrenexNormalForm <21004847+PrenexNormalForm@users.noreply.github.com>
2023-12-02 18:30:09 -0500

    format days 1 & 2

--- day/02/cubes.py
+++ day/02/cubes.py
@@ -1,5 +1,6 @@
 from math import prod
 
+
 class Game:
     def __init__(self, game_string: str):
         self.red_samples, self.green_samples, self.blue_samples = Game.parse(
@@ -7,8 +8,12 @@ class Game:
         )
 
     def allows(self, red: int, green: int, blue: int):
-        return max(self.red_samples) <= red and max(self.green_samples) <= green and max(self.blue_samples) <= blue
-    
+        return (
+            max(self.red_samples) <= red
+            and max(self.green_samples) <= green
+            and max(self.blue_samples) <= blue
+        )
+
     def min(self):
         return max(self.red_samples), max(self.green_samples), max(self.blue_samples)
 
@@ -28,16 +33,17 @@ class Game:
                     blue_samples.add(quantity)
         return red_samples, green_samples, blue_samples
 
-if __name__ == '__main__':
-    with open('day/02/input.txt') as f:
+
+if __name__ == "__main__":
+    with open("day/02/input.txt") as f:
         sum = 0
         min_power_sum = 0
         for line in f:
-            game_string = line.split(':')[-1]
-            game_id = int(line.split(':')[0].split()[-1])
+            game_string = line.split(":")[-1]
+            game_id = int(line.split(":")[0].split()[-1])
             game = Game(game_string)
             if game.allows(12, 13, 14):
                 sum += game_id
             min_power_sum += prod(game.min())
-        print(f'ID sum of games allowing 12 red, 13 green, 14 blue: {sum}')
-        print(f'Sum of game minimum powers: {min_power_sum}')
+        print(f"ID sum of games allowing 12 red, 13 green, 14 blue: {sum}")
+        print(f"Sum of game minimum powers: {min_power_sum}")
